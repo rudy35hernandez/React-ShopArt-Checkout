656bac8bcba86f5b78342f9f1fce6aedf8f3355c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-17 23:27:51 -0700

    added api data to state

--- src/Context.js
+++ src/Context.js
@@ -3,6 +3,13 @@ const Context = React.createContext()
 
 function ContextProvider({children}){
     const[allPhotos, setAllPhotos] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json`)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
     return(
         <Context.Provider value={{allPhotos: allPhotos}}>
             {children}
