531c75e2c0ee5b0db90b2e7fef9627534dbc289c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-05-21 23:12:28 -0700

    added context provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ContextProvider} from "./Context.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <ContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ContextProvider>
   </React.StrictMode>
 );
 
