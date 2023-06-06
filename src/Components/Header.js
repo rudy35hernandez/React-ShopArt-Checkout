6437fb2c12162f3166f2ef43ccf850c0fbfa8efb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-06-06 11:52:28 -0700

    added filled cart function to change icon to indicate empty or not

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {Context} from "../Context.js"
 import {Link} from "react-router-dom"
 
 function Header(){
+
+    const {cartItems} = useContext(Context)
+
+    function cartFilled(){
+        if(cartItems.length > 0){
+            return <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+        } else {
+            return <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        }
+    }
+
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x">CART</i></Link>
+            <Link to="/cart">
+                {cartFilled()}
+            </Link>
         </header>
     )
 }
