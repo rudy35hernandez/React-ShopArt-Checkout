0edd791f932e62f66e2ef8db6ae6367c0b3cbab6
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-06-09 14:13:46 -0700

    added method to add up the total and display on cart page

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -4,16 +4,20 @@ import CartItem from "../Components/CartItem.js"
 
 function Cart(){
     const {cartItems} = useContext(Context)
-    
+
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item}/>
     ))
+
+    const totalCost = cartItems.length * 5.99
+
+    const totalDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
 
     return(
         <main className="cart-page">
             <h1>Check Out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: ${totalDisplay}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
