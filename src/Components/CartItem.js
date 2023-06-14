4bf2b9df80df068b1fa66f6c147608dbdc4ee4ee
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-06-14 00:51:34 -0700

    added hovering to trash icon when state is true

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -2,12 +2,18 @@ import React, {useState, useContext} from "react"
 import {Context} from "../Context"
 
 function CartItem({item}) {
-    const [isHovered, setIsHovered] = useState(true)
+    const [isHovered, setIsHovered] = useState(false)
     const {removeFromCart} = useContext(Context)
+
+    const trashIconClass = isHovered ? `ri-delete-bin-fill` : `ri-delete-bin-line`
     
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
+            <i className={trashIconClass}
+                onMouseLeave={() => setIsHovered(false)}
+                onMouseEnter={() => setIsHovered(true)}
+                onClick={() => removeFromCart(item.id)}>
+            </i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
